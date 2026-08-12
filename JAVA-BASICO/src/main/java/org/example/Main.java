0d22984c933ae8143d43ecb4f5358edb8aa7b777
JulianaS2z <juliana.e.santos@ba.estudante.senai.br>
2026-08-11 21:26:44 -0300

    Java.basic

---
+++ JAVA-BASICO/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        // Declaração de variáveis.
+        String nome = "Juliana";
+        int idade = 22;
+        double peso = 51.0;
+
+        // Declaração de constantes
+        final String CPF = "111.111.111-00";
+
+        System.out.println("O nome do usuário é: " + nome);
+        System.out.println("O CPF do usuário é: " + CPF);
+        System.out.println("A idade do usuário é: " + idade + " anos");
+        System.out.println("O nome do usuário é: " + peso + " quilos");
+    }
+}
