6158067634763baf5bf4a82f307a14558b1f330d
JulianaS2z <juliana.e.santos@ba.estudante.senai.br>
2026-08-17 21:28:28 -0300

    Add files via upload

---
+++ Javabasic/JAVA-BASICO/src/main/java/org/example/Par.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Par {
+    static void main() {
+
+        for (int i = 100; i <= 120; i=i+2){
+            System.out.println("Valor de i:" +i);
+        }
+    }
+
+}
